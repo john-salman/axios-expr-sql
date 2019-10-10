1064b50d5c32a35c7cd6c60b3bceccff1d4cae2f
john-salman <johnfish15@gmail.com>
2019-10-09 17:00:16 -0700

    Fleshed out routes with interaction

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,53 @@
 const app = require('express')();
-const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const port = 3001;
 
-const connection = mysql.createConnection({
-    host: '',
-    user: '',
-    password: '',
-    database: ''
-});
-app.get('/test', (req, res) => {
+const dbConnection = require('./database/connect');
 
-    connection.connect();
+app.use(cors());
 
-    connection.query('SELECT * FROM test_table;', function (error, results, fields) {
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    dbConnection.query('SELECT * FROM test_table;', (error, results) => {
         if (error) {
             console.log(`Error: ${error}`);
         } else {
-            console.log('The result is: ', results);
-            res.send(results[0]);
+            res.send(results);
         }
     });
+});
 
-    connection.end();
+app.post('/new/:name1/:name2', (req, res) => {
+    let name1 = req.params.name1;
+    let name2 = req.params.name2;
+    let query = 'INSERT INTO test_table (name1, name2) VALUES (?, ?);';
+    dbConnection.query({
+        sql: query,
+        values: [name1, name2]
+    }, error => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.redirect('/');
+        }
+    })
+});
+
+app.post('/delete/:id', (req, res) => {
+    console.log("Hit id delete");
+   let query = 'DELETE FROM test_table WHERE id = ?;';
+    dbConnection.query({
+        sql: query,
+        values: [req.params.id]
+    }, error => {
+        if (error) {
+            console.log(`Error: ${error}`);
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,47 +1,80 @@
 import React from 'react';
-import rp from 'request-promise';
-import logo from './logo.svg';
 import './App.css';
+
+import axios from './configAxios';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            names: [],
+            name1: "",
+            name2: ""
+        };
+    }
+
+    updateNames(_response) {
+        var parsedResponse = JSON.parse(JSON.stringify(_response.data));
+        console.log(`DB Response: ${JSON.stringify(parsedResponse)}`);
+        let updated_array = [];
+        parsedResponse.forEach((data_packet) => {
+            let _name = data_packet.name1 + " " + data_packet.name2;
+            let _id = data_packet.id;
+            updated_array = updated_array.concat([{name: _name, id: _id}]);
+        });
+        this.setState({names: JSON.parse(JSON.stringify(updated_array))})
     }
 
     componentDidMount() {
-        rp('/test')// didnt work, need to configure routing through proxy (or switch to axios)
-            .then((results) => {
-                console.log(`Success: ${results}`)
-            })
-            .catch(err => {
-                console.log(`Error: ${err}`)
+        axios.get('/')
+            .then(response => this.updateNames(response))
+            .catch(error => {
+                console.log(`From componentDidMount: ${error}`)
             });
-
-        fetch('/test')
-            .then(response => response.json()) // Apparently you need to parse the wierd response
-            .then(res => {
-                console.log(`Fetch: ${JSON.stringify(res)}`);
-            })
   }
 
+  buttonClick(id){
+        axios.post( `/delete/${id}`)
+            .then(response => this.updateNames(response))
+            .catch(error => console.log(`From buttonClick: ${error}`));
+  }
+
+  handleName1Change = event => {
+        this.setState({
+            newName1: event.target.value
+        })
+  };
+
+  handleName2Change = event => {
+        this.setState({
+            newName2: event.target.value
+        })
+  };
+
+  submitNames() { // axios in this app is having some issues with submitting through raw html, works in pure express though
+      if (this.state.newName1 && this.state.newName2) {
+          axios.post(`/new/${this.state.newName1}/${this.state.newName2}`)
+              .then(response => this.updateNames(response))
+              .catch(error => console.log(`From submitNames: ${error}`));
+      } else {
+          alert("Please enter values before submitting");
+      }
+  };
+
   render () {
+    let names = this.state.names ? this.state.names : [];
     return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+            {names.map((name) =>
+                <p>{name.name}<button onClick={() => this.buttonClick(name.id)}>Delete Me!</button></p>
+            )}
+            <br/>
+            <input value={this.state.newName1} onChange={this.handleName1Change} type="text"/>
+            <input value={this.state.newName2} onChange={this.handleName2Change} type="text"/>
+            <button onClick={() => this.submitNames()}>Submit</button>
         </div>
+
     );
   }
 }
